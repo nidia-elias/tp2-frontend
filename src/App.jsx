bd9dc73feda2d1968520b1bd89f4d1baed94b115
NidiaElias <nidiaelias27@gmail.com>
2026-05-28 21:41:23 -0300

    feat: estructura inicial limpia del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>¡Proyecto Limpio y Listo!</h1>
+      <p>Acá va a ir nuestro Dashboard con la Sidebar fija.</p>
+    </div>
+  )
+}
+
+export default App
