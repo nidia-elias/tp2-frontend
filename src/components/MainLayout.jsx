1ae0add48a7351d1d9d55701468341bad00b5daa
NidiaElias <nidiaelias27@gmail.com>
2026-05-31 01:03:52 -0300

    modifique mainlayout para que se vea el sidebar

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -7,16 +7,21 @@ const MainLayout = () => {
     <div className="dashboard-container">
       
         <h2>Dashboard</h2>
-        <nav>
-          {/*  links */}
-          <p>Menú de Navegación</p>
-        </nav>
+
+        <aside className="sidebar">
+            <nav>
+                <Link to="/perfil/nidia">Nidia</Link>
+                <Link to="/perfil/lourdes">Lourdes</Link>
+                <Link to="/perfil/maiten">Maiten</Link>
+            </nav>
+        </aside>
      
       <main className="main-content">
         {/* agregar contenido
         */}
         <Outlet /> 
       </main>
+
       <footer className="footer-global">
         <p>Equipo de Desarrollo - 2026</p>
       </footer>
